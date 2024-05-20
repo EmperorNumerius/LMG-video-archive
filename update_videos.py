0060619bd191eff58923ef2574bd95efa37806f2
Emperor Numerius <62899740+EmperorNumerius@users.noreply.github.com>
2024-05-20 17:47:50 -0600

    Create update_videos.py

---
+++ update_videos.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import os
+
+API_KEY = os.getenv('YOUTUBE_API_KEY')
+CHANNEL_ID = 'YOUR_CHANNEL_ID'
+
+def get_videos(api_key, channel_id):
+    base_url = 'https://www.googleapis.com/youtube/v3/'
+    url = f'{base_url}search?key={api_key}&channelId={channel_id}&part=snippet,id&order=date&maxResults=50'
+
+    videos = []
+    while True:
+        response = requests.get(url).json()
+        videos.extend(response['items'])
+        if 'nextPageToken' in response:
+            url = f'{base_url}search?key={api_key}&channelId={channel_id}&part=snippet,id&order=date&maxResults=50&pageToken={response["nextPageToken"]}'
+        else:
+            break
+
+    return videos
+
+videos = get_videos(API_KEY, CHANNEL_ID)
+
+with open('videos.json', 'w') as f:
+    json.dump(videos, f, indent=4)
